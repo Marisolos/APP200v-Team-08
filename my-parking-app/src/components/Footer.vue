c9223be02471eca13bcc144c2022b75c5fc16bdf
Kaisa <kbakstad93@gmail.com>
2025-05-30 16:35:33 +0200

    Legger til Footer.vue-komponenten

---
+++ my-parking-app/src/components/Footer.vue
@@ -0,0 +1,43 @@
+<template>
+  <footer class="footer">
+    <div class="footer-left">
+      <p>Ledig plass? Lei den ut. Trenger plass? Book den.</p>
+    </div>
+    <div class="footer-right">
+      <p><router-link to="/faq">FAQ</router-link></p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent"
+};
+</script>
+
+<style scoped>
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #5B8D8A;
+  color: #ffffff;
+  padding: 15px 30px;
+  font-family: "Nunito Sans", sans-serif;
+  font-size: 16px;
+}
+
+.footer a {
+  color: #ffffff;
+  text-decoration: underline;
+}
+
+.footer a:hover {
+  text-decoration: none;
+}
+
+.footer-left p,
+.footer-right p {
+  margin: 0;
+}
+</style>
